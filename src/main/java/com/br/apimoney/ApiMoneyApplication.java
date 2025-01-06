d04a80918dc26c306ad0ac264843b2235cda1d73
jeanheberth <jeanheberth19@gmail.com>
2025-01-06 20:58:18 -0300

    Initial commit

---
+++ src/main/java/com/br/apimoney/ApiMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.br.apimoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMoneyApplication.class, args);
+    }
+
+}
