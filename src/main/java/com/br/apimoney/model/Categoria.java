5aab4abcaa8066d80d2db5306e536a2909cff433
jeanheberth <jeanheberth19@gmail.com>
2025-01-06 23:28:26 -0300

    Corrigindo erro do postman vazio

--- src/main/java/com/br/apimoney/model/Categoria.java
+++ src/main/java/com/br/apimoney/model/Categoria.java
@@ -1,23 +1,31 @@
 package com.br.apimoney.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Table(name = "tb_categoria")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("codigo")
     private Long codigo;
 
     @Column(nullable = false)
+    @JsonProperty("nome")
     private String nome;
+
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/com/br/apimoney/resource/CategoriaResource.java
+++ src/main/java/com/br/apimoney/resource/CategoriaResource.java
@@ -1,9 +1,22 @@
 package com.br.apimoney.resource;
 
+import com.br.apimoney.model.Categoria;
+import com.br.apimoney.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @RequestMapping
+    public List<Categoria> categorias(){
+        return categoriaRepository.findAll();
+    }
 }
