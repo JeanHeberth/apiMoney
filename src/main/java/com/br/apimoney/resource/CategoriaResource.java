291db3254ceed03d0745b623d3101880f4dde7e5
jeanheberth <jeanheberth19@gmail.com>
2025-01-10 19:48:10 -0300

    Implementado ExceptionHandler

--- src/main/java/com/br/apimoney/resource/CategoriaResource.java
+++ src/main/java/com/br/apimoney/resource/CategoriaResource.java
@@ -2,11 +2,17 @@ package com.br.apimoney.resource;
 
 import com.br.apimoney.model.Categoria;
 import com.br.apimoney.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -16,7 +22,28 @@ public class CategoriaResource {
     private CategoriaRepository categoriaRepository;
 
     @RequestMapping
-    public List<Categoria> categorias(){
+    public List<Categoria> categorias() {
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> salvar(@Validated @RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        if (categoriaSalva.getNome() == null || categoriaSalva.getNome().isEmpty()) {
+            return ResponseEntity.badRequest().body("O campo nome é obrigatório");
+        }
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
     }
 }
